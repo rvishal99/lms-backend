b33202ba670fa20069292a913307ba0ad2adefee
rvishal99 <r.vishal7777777@gmail.com>
2024-02-13 00:45:31 +0530

    Added Correct paths

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { json } from "express";
+// import { json } from "express";
 import AppError from "../Utilities/error.util.js";
 import User from "../models/user.model.js";
 import cloudinary from "cloudinary"
